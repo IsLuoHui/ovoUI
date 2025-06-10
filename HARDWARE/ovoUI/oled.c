f255d119384fd9a52f88b82821686c1f48e00bb6
IsLuoHui <isluohui@outlook.com>
2025-06-10 22:04:02 +0800

    OLED驱动层和应用层解耦

--- HARDWARE/ovoUI/oled.c
+++ HARDWARE/ovoUI/oled.c
@@ -2,14 +2,277 @@
 #include "spi.h"
 #include <stdlib.h>
 #include "string.h"
+#include "font.h"
 
 
 // **全局 OLED 显示缓冲区**
-u8 framebuffer[1024] = {0};
+u8 FrameBuffer[1024] = {0};
+
+static u16 RAMCursor;
 
 ELEMENT *elementPtrs[MAX_ELEMENTS] = {0}; // 存储动态创建的 ELEMENT 指针
 u8 elementCount = 0; // 当前元素数量
 
+
+void OLED_RAM_Refresh(u8 *RAM) {
+    u8 x, y;
+    RAMCursor = 0;
+    for (y = 0;y < 8;y++)
+    {
+        OLED_Set_Cursor(0, y);
+        for (x = 0;x < 128;x++)OLED_W_DATA(RAM[RAMCursor++]);
+    }
+}
+
+void OLED_RAM_Clear(u8 *RAM) {
+    memset(RAM, 0, OLED_SIZE);
+}
+
+void OLED_Fill_RAM(u8 *RAM) {
+    for(RAMCursor=0;RAMCursor<OLED_SIZE;RAMCursor++)RAM[RAMCursor]=0xFF;
+}
+
+void OLED_DrawPoint(u8 x, u8 y, u8 *RAM, u8 draw) {
+    if (x > OLED_WIDTH - 1 || y > OLED_HEIGHT * 8 - 1)return;
+    switch(draw){
+        case 0:
+            RAM[y/8*128+x]&=~(0x01<<(y%8));
+            break;
+        case 1:
+            RAM[y/8*128+x]|=0x01<<(y%8);
+            break;
+        case 2:
+            if(RAM[y/8*128+x]>>(y%8)&0x01)RAM[y/8*128+x]&=~(0x01<<(y%8));
+            else RAM[y/8*128+x]|=0x01<<(y%8);
+            break;
+    }
+}
+
+void OLED_DrawLine(u8 x0, u8 y0, u8 x1, u8 y1, u8 *RAM, u8 draw) {
+    u8 dx =abs(x1-x0), dy =abs(y1-y0);
+    char sx =x0<x1?1:-1, sy =y0<y1?1:-1; 
+    short err =(dx>dy?dx:-dy)/2, e2;
+    while(1){
+        OLED_DrawPoint(x0, y0, RAM, draw);
+        if(x0==x1&&y0==y1) break;
+        e2=err;
+        if(e2>-dx){
+            err-=dy;
+            x0+=sx;
+        }
+        if(e2<dy){
+            err+=dx;
+            y0+=sy;
+        }
+    }
+}
+
+void OLED_DrawDashedLine(u8 x0, u8 y0, u8 x1, u8 y1, u8 dashlen, u8 *RAM, u8 draw) {
+    u8 dx = abs(x1 - x0), dy = abs(y1 - y0);
+    int8_t sx = x0 < x1 ? 1 : -1, sy = y0 < y1 ? 1 : -1;;
+    int16_t err = (dx > dy ? dx : -dy) / 2, e2;
+    u8 count =0;
+    
+    while(1){
+        //间隔与线端等长
+        if(count<dashlen)OLED_DrawPoint(x0, y0, RAM, draw);
+        count =(count+1)%(2*dashlen);
+        //间隔画点
+        //if(count%dashlen==0)OLED_DrawPoint(x0, y0, RAM, draw);
+        //count++;
+        
+        if(x0==x1&&y0==y1) break;
+        e2=err;
+        if(e2>-dx){
+            err-=dy;
+            x0+=sx;
+        }
+        if(e2<dy){
+            err+=dx;
+            y0+=sy;
+        }
+    }
+}
+
+void OLED_DrawRect(u8 x0, u8 y0, u8 x1, u8 y1, u8 *RAM, u8 draw) {
+    OLED_DrawLine(x0, y0, x0, y1, RAM, draw);
+    OLED_DrawLine(x0,y0,x1,y0,RAM,draw);
+    OLED_DrawLine(x1,y1,x0,y1,RAM,draw);
+    OLED_DrawLine(x1,y1,x1,y0,RAM,draw);
+}
+
+void OLED_DrawFillRect(u8 x0, u8 y0, u8 x1, u8 y1, u8 *RAM, u8 draw) {
+    u8 i, j;
+    if(x0>x1){i=x1;x1=x0;x0=i;}
+    if(y0>y1){i=y1;y1=y0;y0=i;}
+    for(i=x0;i<x1;i++)
+    {
+        for(j=y0;j<y1;j++)OLED_DrawPoint(i,j,RAM,draw);
+    }
+}
+
+u32 OLED_POW(u8 m,u8 n)
+{
+	u32 result=1;	 
+	while(n--)result*=m;    
+	return result;
+}
+
+void OLED_ShowChar(u8 x, u8 page, char c, u8 *RAM, u8 draw) {
+    u8 i;
+    switch(draw){
+        case 0:
+            for (i = 0; i < 8; i++)
+            {
+                RAM[page*128+x+i]&=~ASCII_8X16[c - ' '][i];
+            }
+            for (i = 0; i < 8; i++)
+            {
+                RAM[(page+1)*128+x+i]&=~ASCII_8X16[c - ' '][i+8];
+            }
+            break;
+        case 1:
+            for (i = 0; i < 8; i++)
+            {
+                RAM[page*128+x+i]|=ASCII_8X16[c - ' '][i];
+            }
+            for (i = 0; i < 8; i++)
+            {
+                RAM[(page+1)*128+x+i]|=ASCII_8X16[c - ' '][i+8];
+            }
+            break;
+        case 2:
+            for (i = 0; i < 8; i++)
+            {
+                RAM[page*128+x+i]=ASCII_8X16[c - ' '][i];
+            }
+            for (i = 0; i < 8; i++)
+            {
+                RAM[(page+1)*128+x+i]=ASCII_8X16[c - ' '][i+8];
+            }
+            break;
+    }
+
+}
+
+void OLED_ShowString(u8 x, u8 page, char *str, u8 *RAM, u8 draw) {
+    u8 i;
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        OLED_ShowChar(x + i * 8, page, str[i], RAM, draw);
+    }
+}
+
+void OLED_ShowNum(u8 x, u8 page, u32 num, u8 len, u8 *RAM, u8 draw) {
+    u8 i;
+    for (i = 0; i < len; i++)                            
+    {
+        OLED_ShowChar(x+i*8, page, num / OLED_POW(10, len - i - 1) % 10 + '0', RAM ,draw);
+    }
+}
+
+void OLED_ShowHexNum(u8 x, u8 page, u32 num, u8 len, u8 *RAM, u8 draw) {
+    u8 i, SingleNumber;
+    for (i = 0; i < len; i++)                            
+    {
+        SingleNumber = num / OLED_POW(16, len - i - 1) % 16;
+        if (SingleNumber < 10)
+        {
+            OLED_ShowChar(x + i * 8, page, SingleNumber + '0', RAM ,draw);
+        }
+        else
+        {
+            OLED_ShowChar(x + i * 8, page, SingleNumber - 10 + 'A', RAM ,draw);
+        }
+    }
+}
+
+void OLED_ShowCN(u8 x, u8 page, u8 CC[], u8 *RAM, u8 draw) {
+    u8 m, n;
+    u8 i=0;
+    u8 fontnum=0;
+    while(GB2312_16X16[fontnum].Index[0]!='\0'){
+        if(CC[0]==GB2312_16X16[fontnum].Index[0]&&CC[1]==GB2312_16X16[fontnum].Index[1]){
+            for(n=0;n<2;n++)
+            {
+                for(m=0;m<16;m++)
+                {
+                    switch(draw){
+                        case 0:
+                            RAM[(page+n)*128+x+m]&=~GB2312_16X16[fontnum].Data[i++];
+                            break;
+                        case 1:
+                            RAM[(page+n)*128+x+m]|=GB2312_16X16[fontnum].Data[i++];
+                            break;
+                        case 2:
+                            RAM[(page+n)*128+x+m]=GB2312_16X16[fontnum].Data[i++];
+                            break;
+                    }
+                }    
+            }
+            return;
+        }else
+        {
+            fontnum++;
+        }
+    }
+    OLED_ShowString(x,page,"??", RAM, draw);
+}
+
+void OLED_ShowCNString(u8 x, u8 page, char *String, u8 *RAM, u8 draw) {
+    u8 i;
+    u8 c[] ={0,0};
+    for (i = 0; String[i] != '\0'; i+=2)
+    {
+        c[0]=String[i];
+        c[1]=String[i+1];
+        OLED_ShowCN(x + i*8, page, c, RAM, draw);
+    }
+}
+
+void OLED_ShowBMP(u8 x, u8 page, u8 w, u8 h, u8 BMP[], u8 *RAM, u8 draw)
+{
+    u32 i=0;
+    u8 j,k;
+    for(j=page;j<j+h;j++)
+    {
+        for(k=x;k<x+w;k++)
+        {
+            switch(draw){
+                case 0:
+                    RAM[j*128+k]&=~BMP[i++];
+                    break;
+                case 1:
+                    RAM[j*128+k]|=BMP[i++];
+                    break;
+                case 2:
+                    RAM[j*128+k]=BMP[i++];
+                    break;
+            }
+        }
+    }
+}
+
+void OLED_ShowMixString(u8 x, u8 page, char *String, u8 *RAM, u8 draw)
+{
+    u8 i;
+    u8 c[] ={0,0};
+    for (i = 0; String[i] != '\0';)
+    {
+        if(String[i]<=127)
+        {
+            OLED_ShowChar(x+i*8, page, String[i], RAM, draw);
+            i++;
+        }
+        else
+        {
+            c[0]=String[i];
+            c[1]=String[i+1];
+            OLED_ShowCN(x + i*8, page, c, RAM, draw);
+            i+=2;
+        }
+    }
+}
 
 // **创建 ELEMENT（动态分配）**
 ELEMENT *OLED_Create_Element(int16_t x, int16_t y, u8 w, u8 h,u8 mix, u8 *data) {
@@ -43,7 +306,6 @@ void modifyElement(ELEMENT *ele, int16_t x, int16_t y, u8 w, u8 h, u8 *data, u8 
 // **删除 ELEMENT**
 void removeElement(u8 index) {
     if (index >= elementCount || !elementPtrs[index]) return;
-
     free(elementPtrs[index]); // 释放内存
     for (u8 i = index; i < elementCount - 1; i++) {
         elementPtrs[i] = elementPtrs[i + 1]; // 移动指针
@@ -52,11 +314,8 @@ void removeElement(u8 index) {
     elementCount--;
 }
 
-/**
- * x y w h mix data
- */
 void OLED_Mix_Print() {
-    memset(framebuffer, 0, 1024);
+    memset(FrameBuffer, 0, 1024);
     for (u8 n = 0; n < elementCount; n++) {
         ELEMENT *e = elementPtrs[n];
         if (!e) continue;
@@ -100,7 +359,7 @@ void OLED_Mix_Print() {
 
                 int16_t fb_page = page_start + page;
                 if (col >= 0 && col < WIDTH && fb_page >= 0 && fb_page < HEIGHT / 8)
-                    framebuffer[fb_page * WIDTH + col] = out;
+                    FrameBuffer[fb_page * WIDTH + col] = out;
             }
         }
 
@@ -108,28 +367,14 @@ void OLED_Mix_Print() {
         int16_t py = e->y;
         if (px < 0) {
             px = -px;
-            OLED_GShowChar(0, 6, '-', framebuffer, 1);
+            OLED_ShowChar(0, 6, '-', FrameBuffer, 1);
         }
         if (py < 0) {
             py = -py;
-            OLED_GShowChar(0, 4, '-', framebuffer, 1);
+            OLED_ShowChar(0, 4, '-', FrameBuffer, 1);
         }
-        OLED_GShowNum(8, 4, py, 3, framebuffer, 1);
-        OLED_GShowNum(8, 6, px, 3, framebuffer, 1);
+        OLED_ShowNum(8, 4, py, 3, FrameBuffer, 1);
+        OLED_ShowNum(8, 6, px, 3, FrameBuffer, 1);
     }
-    OLED_RAM_Refresh(framebuffer);
+    OLED_RAM_Refresh(FrameBuffer);
 }
- /*
-        int16_t px = e->x;
-        int16_t py = e->y;
-        if (px < 0) {
-            px = -px;
-            OLED_GShowChar(0, 6, '-', framebuffer, 1);
-        }
-        if (py < 0) {
-            py = -py;
-            OLED_GShowChar(0, 4, '-', framebuffer, 1);
-        }
-        OLED_GShowNum(8, 4, py, 3, framebuffer, 1);
-        OLED_GShowNum(8, 6, px, 3, framebuffer, 1);
-        */
--- HARDWARE/ovoUI/oled.h
+++ HARDWARE/ovoUI/oled.h
@@ -2,20 +2,46 @@
 #define __OLED_H
 #include "stm32f10x.h"
 
-// **定义 OLED 屏幕尺寸**
+// *OLED 屏幕尺寸(Pixel)
 #define WIDTH 128
 #define HEIGHT 64
 #define MAX_ELEMENTS 20 // 最大元素数量
 
-// **定义 ELEMENT 结构体**
+// *ELEMENT 结构体
 typedef struct {
-    int16_t x, y;   // 元素位置（像素单位）
-    u8 w, h;   // 元素宽度、高度（像素单位）
-    u8 mix;    // 显示模式 0x00:覆盖 0x01:叠加 0x02:反色
-    u8 *data;  // 显示数据指针
+    int16_t x, y;
+    u8 w, h;
+    u8 mix;
+    u8 *data;
 } ELEMENT;
 
-ELEMENT *OLED_Create_Element(int16_t x, int16_t y, u8 w, u8 h,u8 mix, u8 *data);
+//RAM
+void OLED_RAM_Refresh(u8 *RAM);
+void OLED_RAM_Clear(u8 *RAM);
+void OLED_Fill_RAM(u8 *RAM);
+void OLED_DrawPoint(u8 x, u8 y, u8 *RAM, u8 draw);
+void OLED_DrawLine(u8 x0, u8 y0, u8 x1, u8 y1, u8 *RAM, u8 draw);
+void OLED_DrawDashedLine(u8 x0, u8 y0, u8 x1, u8 y1, u8 dashlen, u8 *RAM, u8 draw);
+void OLED_DrawRect(u8 x0, u8 y0, u8 x1, u8 y1, u8 *RAM, u8 draw);
+void OLED_DrawFillRect(u8 x0, u8 y0, u8 x1, u8 y1, u8 *RAM, u8 draw);
+
+void OLED_ShowChar(u8 x, u8 page, char c, u8 *RAM, u8 draw);
+void OLED_ShowString(u8 x, u8 page, char *str, u8 *RAM, u8 draw);
+void OLED_ShowNum(u8 x, u8 page, u32 num, u8 len, u8 *RAM, u8 draw);
+void OLED_ShowHexNum(u8 x, u8 page, u32 num, u8 len, u8 *RAM, u8 draw);
+void OLED_ShowCN(u8 x, u8 page, u8 CC[], u8 *RAM, u8 draw);
+void OLED_ShowCNString(u8 x, u8 page, char *String, u8 *RAM, u8 draw);
+void OLED_ShowBMP(u8 x, u8 page, u8 w, u8 h, u8 BMP[], u8 *RAM, u8 draw);
+void OLED_ShowMixString(u8 x, u8 page, char *String, u8 * RAM, u8 draw);
+
+//宽高 W & H
+#define OLED_WIDTH 128  //byte
+#define OLED_HEIGHT 8   //page
+
+#define OLED_SIZE (OLED_WIDTH * OLED_HEIGHT)
+
+
+ELEMENT *OLED_Create_Element(int16_t x, int16_t y, u8 w, u8 h, u8 mix, u8 *data);
 void modifyElement(ELEMENT *ele, int16_t x, int16_t y, u8 w, u8 h, u8 *data, u8 mix);
 void removeElement(u8 index);
 
